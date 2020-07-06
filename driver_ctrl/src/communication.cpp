4b57d0fcd503e037bf7250bc12960bb9ed04f9c7
MJY1996 <1977415395@qq.com>
2020-07-06 21:03:25 +0800

    add notes to driver controller

--- driver_ctrl/src/communication.cpp
+++ driver_ctrl/src/communication.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file communication.cpp
+ * @author MJY1996 (1977415395@qq.com)
+ * @brief serial communication node for tricycle controller
+ * @version 0.1
+ * @date 2020-07-06
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
 #include <ros/ros.h>
 #include <serial/serial.h>
 
@@ -6,29 +16,50 @@
 
 #include <stdint.h>
 
+/**
+ * @brief serial port for communicating with tricycle
+ * 
+ */
 serial::Serial ser;
 
+/**
+ * @brief command key
+ * 
+ */
 enum CtrlCmd
 {
-    cmd_car_state = 0x00,
-    cmd_speed_ctrl = 0x20,
-    cmd_gyro_ctrl = 0x21,
-    cmd_set_odometry_l = 0x50,
-    cmd_set_odometry_r = 0x51,
-    cmd_set_yaw = 0x52,
+    cmd_car_state = 0x00,      // control tricycle on or off
+    cmd_speed_ctrl = 0x20,     // control linear velocity
+    cmd_gyro_ctrl = 0x21,      // control angular velocity
+    cmd_set_odometry_l = 0x50, // set odometry value of left wheel
+    cmd_set_odometry_r = 0x51, // set odometry value of right wheel
+    cmd_set_yaw = 0x52,        // set yaw angle
 };
 
+/**
+ * @brief control value for command kye cmd_car_state
+ * 
+ */
 enum CarStateCmd
 {
-    car_state_cmd_stop = 0x00,
-    car_state_cmd_start = 0x11,
-    car_state_cmd_heartbeat = 0x22,
+    car_state_cmd_stop = 0x00,      // turn on the controller
+    car_state_cmd_start = 0x11,     // turn off the controller
+    car_state_cmd_heartbeat = 0x22, // periodically send heartbeat data
+                                    // incase the controller turns off after
+                                    // 1 second receive no datas
 };
 
-const uint16_t MSG_HEAD = 0xeb90;
-const size_t TX_SIZE = 8;
-const size_t RX_SIZE = 47;
+const uint16_t MSG_HEAD = 0xeb90; // head word of data
+const size_t TX_SIZE = 8;         // size of data sending to controller
+const size_t RX_SIZE = 47;        // size of data receiving from controller
 
+/**
+ * @brief copy data
+ * 
+ * @param from source address
+ * @param to target address
+ * @param size size of data
+ */
 void CopyMsg(uint8_t *from, uint8_t *to, uint8_t size)
 {
     while (size--)
@@ -37,6 +68,13 @@ void CopyMsg(uint8_t *from, uint8_t *to, uint8_t size)
     }
 }
 
+/**
+ * @brief calculate the check byte using sum check
+ * 
+ * @param msg data to be checked
+ * @param size size of data
+ * @return uint8_t sum check byte
+ */
 uint8_t CalcCheck(uint8_t *msg, uint8_t size)
 {
     uint8_t sum = 0;
@@ -47,6 +85,15 @@ uint8_t CalcCheck(uint8_t *msg, uint8_t size)
     return sum;
 }
 
+/**
+ * @brief check if the datas conforms to the format
+ *        according to its header and check byte
+ * 
+ * @param msg datas to be checked
+ * @param size size of datas
+ * @return true the datas conforms to the format
+ * @return false the datas coes not conform to the format
+ */
 bool CheckFormat(uint8_t *msg, uint8_t size)
 {
     if ((msg[0] != static_cast<uint8_t>(MSG_HEAD >> 8)) ||
@@ -66,6 +113,12 @@ bool CheckFormat(uint8_t *msg, uint8_t size)
     }
 }
 
+/**
+ * @brief send control command
+ * 
+ * @param cmd key of command
+ * @param data value of command
+ */
 void TxCmd(CtrlCmd cmd, float data)
 {
     uint8_t tx_data[TX_SIZE];
@@ -77,6 +130,12 @@ void TxCmd(CtrlCmd cmd, float data)
     ser.write(tx_data, TX_SIZE);
 }
 
+/**
+ * @brief send command to control car state
+ * 
+ * @param cmd key of commane
+ * @param state value of command
+ */
 void TxCmd(CtrlCmd cmd, CarStateCmd state)
 {
     uint8_t tx_data[TX_SIZE];
@@ -88,6 +147,15 @@ void TxCmd(CtrlCmd cmd, CarStateCmd state)
     ser.write(tx_data, TX_SIZE);
 }
 
+/**
+ * @brief control car according to the subscribed service
+ * 
+ * @param req request of the service, including 
+ *            linear velocity and angular velocity
+ * @param res response of the service, null for this service
+ * @return true successfully handle the request
+ * @return false 
+ */
 bool serial_tx_handle_function(driver_ctrl::ctrl_msg::Request &req,
                                driver_ctrl::ctrl_msg::Response &res)
 {
@@ -96,22 +164,31 @@ bool serial_tx_handle_function(driver_ctrl::ctrl_msg::Request &req,
     return true;
 }
 
-
 uint8_t buffer[3 * RX_SIZE];
-uint16_t end_bias = 0;
-bool serial_rx_function(driver_ctrl::fb_msg  &msg)
+uint16_t end_flag = 0;
+
+/**
+ * @brief decode datas received from the controller, if there is a complete
+ *        set of data in buffer, wite data in message and return true
+ * 
+ * @param msg pointer of the message to be released,
+ *            including state feedback of the tricycle
+ * @return true if the buffer contains a complete set of data
+ * @return false 
+ */
+bool serial_rx_function(driver_ctrl::fb_msg &msg)
 {
     size_t n = ser.available();
     if (n == 0)
     {
         return false;
     }
-    n = ser.read(buffer + end_bias, n);
-    end_bias += n;
-    uint16_t head_bias = 0;
-    while ((end_bias - head_bias) >= RX_SIZE)
+    n = ser.read(buffer + end_flag, n);
+    end_flag += n;
+    uint16_t head_flag = 0;
+    while ((end_flag - head_flag) >= RX_SIZE)
     {
-        if (CheckFormat(buffer + head_bias, RX_SIZE))
+        if (CheckFormat(buffer + head_flag, RX_SIZE))
         {
             union TmpData
             {
@@ -151,57 +228,34 @@ bool serial_rx_function(driver_ctrl::fb_msg  &msg)
             i += 4;
             CopyMsg(buffer + i, tmp.u8, 4);
             msg.vin = tmp.f32;
-            for (i = 0; i < (end_bias - head_bias - RX_SIZE); i++)
+            for (i = 0; i < (end_flag - head_flag - RX_SIZE); i++)
             {
-                buffer[i] = buffer[head_bias + RX_SIZE];
+                buffer[i] = buffer[head_flag + RX_SIZE];
             }
-            end_bias = end_bias - head_bias - RX_SIZE;
+            end_flag = end_flag - head_flag - RX_SIZE;
             return true;
         }
         else
         {
-            head_bias++;
+            head_flag++;
         }
     }
-    for (int i = 0; i < (end_bias - head_bias); i++)
+    for (int i = 0; i < (end_flag - head_flag); i++)
     {
-        buffer[i] = buffer[head_bias];
+        buffer[i] = buffer[head_flag];
     }
-    end_bias = end_bias - head_bias;
+    end_flag = end_flag - head_flag;
     return false;
-
 }
 
-// bool serial_rx_function(serial_demo::serial_rx &msg)
-// {
-//     size_t n = ser.available();
-//     if (n == 0)
-//     {
-//         return false;
-//     }
-//     else
-//     {
-//         uint8_t buffer[10];
-//         n = ser.read(buffer, n);
-//         if (n == 7) 
-//         {
-//             msg.x_array = buffer[2];
-//             msg.y_array = buffer[3];
-//             // msg.buttom_a = (buffer[4]) && 0x01;
-//             // msg.buttom_b = (buffer[4] >> 1) && 0x01;
-//             // msg.buttom_c = (buffer[4] >> 2) && 0x01;
-//             return true;
-//         }
-//         else
-//         {
-//             ROS_INFO("message size = %d", static_cast<int>(n));
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-
-int main(int argc, char** argv)
+/**
+ * @brief 
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int main(int argc, char **argv)
 {
     ros::init(argc, argv, "communication");
     ros::NodeHandle nh;
@@ -214,7 +268,7 @@ int main(int argc, char** argv)
         ser.setTimeout(t);
         ser.open();
     }
-    catch(serial::IOException& e)
+    catch (serial::IOException &e)
     {
         ROS_ERROR_STREAM("Failed to open serial port");
         return -1;
@@ -226,7 +280,7 @@ int main(int argc, char** argv)
     else
     {
         return -1;
-    }    
+    }
 
     TxCmd(cmd_car_state, car_state_cmd_start);
     driver_ctrl::fb_msg feedback_msg;
